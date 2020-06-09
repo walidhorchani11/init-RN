9354af216a8c817dc9b8a55f1e13f1e4efd95cff
walid horchani <walid.horchani@oyez.fr>
2020-06-09 09:21:15 +0100

    manage rendering with state of form page

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ImgPicker from './components/ImgPicker';
 
 export default function App() {
+  const [addProductForm, setAddProductForm] = useState(false);
+  const addProductShow = () => {
+    console.log('add ref cliqued');
+    setAddProductForm(true);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {!addProductForm ? (
+        <ImgPicker addProduct={addProductShow} />
+      ) : (
+        <Text>form page</Text>
+      )}
     </View>
   );
 }
